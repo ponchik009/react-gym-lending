c039c9205946bceee592dc61f074beccd564af05
ponchik009 <lolikonru@mail.ru>
2022-11-08 22:36:44 +0800

    blurs

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -32,11 +32,11 @@ const Join = () => {
         <hr />
         <div>
           <span className="stroke-text">READY TO</span>
-          <span>LEVEL UP</span>
+          <span> LEVEL UP</span>
         </div>
         <div>
           <span>YOUR BODY</span>
-          <span className="stroke-text">WITH US?</span>
+          <span className="stroke-text"> WITH US?</span>
         </div>
       </div>
       <div className="right-j">
